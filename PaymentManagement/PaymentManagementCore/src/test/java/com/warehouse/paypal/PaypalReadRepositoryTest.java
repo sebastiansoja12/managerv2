3f8f22c6e88fd8cdf2a45c884dbfd39e44ef6533
Sebastian Soja <sebastian5253@wp.pl>
2023-02-22 23:49:07 +0100

    Prepare Payment module for new tests

--- PaymentManagement/PaymentManagementCore/src/test/java/com/warehouse/paypal/PaypalReadRepositoryTest.java
+++ PaymentManagement/PaymentManagementCore/src/test/java/com/warehouse/paypal/PaypalReadRepositoryTest.java
@@ -1,10 +1,10 @@
 package com.warehouse.paypal;
 
-import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.warehouse.paypal.configuration.PaypalTestConfiguration;
-import com.warehouse.paypal.infrastructure.adapter.secondary.entity.PaypalEntity;
 import com.warehouse.paypal.infrastructure.adapter.secondary.PaypalReadRepository;
+import com.warehouse.paypal.infrastructure.adapter.secondary.entity.PaypalEntity;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +14,18 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 @ContextConfiguration(classes = PaypalTestConfiguration.class)
-@TestExecutionListeners( {
-        DependencyInjectionTestExecutionListener.class,
-        TransactionDbUnitTestExecutionListener.class,
-})
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class })
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 public class PaypalReadRepositoryTest {
 
--- PaymentManagement/PaymentManagementCore/src/test/java/com/warehouse/paypal/PaypalRepositoryTest.java
+++ PaymentManagement/PaymentManagementCore/src/test/java/com/warehouse/paypal/PaypalRepositoryTest.java
@@ -1,7 +1,7 @@
 package com.warehouse.paypal;
 
 
-import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.warehouse.paypal.configuration.PaypalTestConfiguration;
 import com.warehouse.paypal.domain.model.PaymentInformation;
@@ -15,16 +15,18 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 @ContextConfiguration(classes = PaypalTestConfiguration.class)
-@TestExecutionListeners( {
-        DependencyInjectionTestExecutionListener.class,
-        TransactionDbUnitTestExecutionListener.class,
-})
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class })
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 public class PaypalRepositoryTest {
 
