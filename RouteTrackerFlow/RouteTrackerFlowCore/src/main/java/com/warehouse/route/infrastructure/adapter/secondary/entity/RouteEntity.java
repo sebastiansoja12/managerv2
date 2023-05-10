c901b59db352b3b7af2665b5029b9d01948c0410
Sebastian <sebastian5253@wp.pl>
2023-05-10 19:47:32 +0200

    Add columns adnotation in RouteEntity

--- RouteTrackerFlow/RouteTrackerFlowCore/src/main/java/com/warehouse/route/infrastructure/adapter/secondary/entity/RouteEntity.java
+++ RouteTrackerFlow/RouteTrackerFlowCore/src/main/java/com/warehouse/route/infrastructure/adapter/secondary/entity/RouteEntity.java
@@ -1,7 +1,6 @@
 package com.warehouse.route.infrastructure.adapter.secondary.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import org.hibernate.annotations.Type;
 
@@ -26,26 +25,32 @@ public class RouteEntity {
     @Id
     @GeneratedValue()
     @Type(type = "uuid-char")
+    @Column(name = "id", nullable = false)
     private UUID id;
 
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    @Column(name = "created", nullable = false)
     private LocalDateTime created;
 
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @Column(name = "user_id")
     private UserEntity user;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parcel_id", referencedColumnName = "id")
+    @Column(name = "parcel_id", nullable = false)
     private ParcelEntity parcel;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "depot_id", referencedColumnName = "id")
+    @Column(name = "depot_id")
     private DepotEntity depot;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "supplier_id", referencedColumnName = "id")
+    @Column(name = "supplier_id")
     private SupplierEntity supplier;
 }
 
