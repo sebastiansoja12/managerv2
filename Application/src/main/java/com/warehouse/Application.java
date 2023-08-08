2020711ad0471dc0ffec8bf1152d12ff0e828726
Sebastian <sebastian5253@wp.pl>
2023-08-08 20:37:08 +0200

    [INPL-9606] Add RSA key properties configuration
    [INP-238]

--- Application/src/main/java/com/warehouse/Application.java
+++ Application/src/main/java/com/warehouse/Application.java
@@ -1,13 +1,16 @@
 package com.warehouse;
 
+import com.warehouse.auth.configuration.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @EntityScan(basePackages = {"com.warehouse.*"})
 @EnableJpaRepositories(basePackages = {"com.warehouse.*"})
 @ConfigurationPropertiesScan(basePackages = {"com.warehouse.*"})
