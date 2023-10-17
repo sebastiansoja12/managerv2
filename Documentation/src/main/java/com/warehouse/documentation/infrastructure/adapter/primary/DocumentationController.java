b9a927d0c20734a47c253d26d014b0967d0765e3
Sebastian <sebastian5253@wp.pl>
2023-10-17 23:53:20 +0200

    Add Documentation module

---
+++ Documentation/src/main/java/com/warehouse/documentation/infrastructure/adapter/primary/DocumentationController.java
@@ -0,0 +1,9 @@
+package com.warehouse.documentation.infrastructure.adapter.primary;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/documentations")
+public class DocumentationController {
+}
