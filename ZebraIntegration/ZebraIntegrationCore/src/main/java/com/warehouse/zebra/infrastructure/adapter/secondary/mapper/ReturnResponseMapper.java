1d462e2200ebc3ff594a0d2c28ea919073cb02ab
Sebastian <sebastian5253@wp.pl>
2023-10-31 00:30:23 +0100

    Add ReturnResponseMapper

---
+++ ZebraIntegration/ZebraIntegrationCore/src/main/java/com/warehouse/zebra/infrastructure/adapter/secondary/mapper/ReturnResponseMapper.java
@@ -0,0 +1,12 @@
+package com.warehouse.zebra.infrastructure.adapter.secondary.mapper;
+
+import com.warehouse.zebra.domain.vo.ProcessReturn;
+import com.warehouse.zebra.infrastructure.api.dto.ProcessReturnDto;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ReturnResponseMapper {
+    List<ProcessReturn> map(List<ProcessReturnDto> processReturns);
+}
