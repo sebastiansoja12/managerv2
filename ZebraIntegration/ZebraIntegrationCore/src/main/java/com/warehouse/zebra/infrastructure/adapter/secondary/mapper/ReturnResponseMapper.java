f646816a0f34670d2dcf652cf11d8af8ee1823be
Sebastian <sebastian5253@wp.pl>
2023-11-21 00:43:46 +0100

    Create new objects for secondary api adapters

--- ZebraIntegration/ZebraIntegrationCore/src/main/java/com/warehouse/zebra/infrastructure/adapter/secondary/mapper/ReturnResponseMapper.java
+++ ZebraIntegration/ZebraIntegrationCore/src/main/java/com/warehouse/zebra/infrastructure/adapter/secondary/mapper/ReturnResponseMapper.java
@@ -1,7 +1,7 @@
 package com.warehouse.zebra.infrastructure.adapter.secondary.mapper;
 
 import com.warehouse.zebra.domain.vo.ProcessReturn;
-import com.warehouse.zebra.infrastructure.api.dto.ProcessReturnDto;
+import com.warehouse.zebra.infrastructure.adapter.secondary.api.ProcessReturnDto;
 import org.mapstruct.Mapper;
 
 import java.util.List;
