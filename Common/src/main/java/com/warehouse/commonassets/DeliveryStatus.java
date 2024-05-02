cdb152dbbb36b0239f0d3bbfaff57f8eb06d0407
Sebastian Soja <sebastian5253@wp.pl>
2024-05-02 18:54:01 +0200

    Add DeliveryStatus to commons

---
+++ Common/src/main/java/com/warehouse/commonassets/DeliveryStatus.java
@@ -0,0 +1,14 @@
+package com.warehouse.commonassets;
+
+
+public enum DeliveryStatus {
+    DEPOT,
+    CLIENT,
+    DELIVERY,
+    SENDER,
+    LOST,
+    UNKNOWN,
+    DELIVERED,
+    REJECTED,
+    RETURN
+}
