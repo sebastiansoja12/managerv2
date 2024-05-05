32881033b07a495d921c6c23255332a0d88e328b
Sebastian Soja <sebastian5253@wp.pl>
2024-05-05 21:26:34 +0200

    Add Size to commons

---
+++ Common/src/main/java/com/warehouse/commonassets/Size.java
@@ -0,0 +1,24 @@
+package com.warehouse.commonassets;
+
+
+import lombok.Getter;
+
+@Getter
+public enum Size {
+    TINY("5cmx5cmx5cm", 10),
+    SMALL("10cmx10cmx10cm", 15),
+    MEDIUM("30cmx30cmx30cm", 20),
+    AVERAGE("50cmx50cmx50cm", 30),
+    BIG("80cmx80cmx80cm", 50),
+    CUSTOM("XcmXcmXcm", 70),
+    TEST("test", 99);
+
+    private final String parcelSize;
+
+    private final double price;
+
+    Size(String size, double price) {
+        this.parcelSize = size;
+        this.price = price;
+    }
+}
