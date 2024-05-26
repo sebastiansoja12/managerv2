9d25fa33c742bc85655b4d369b0152bdd6b13560
Sebastian Soja <sebastian5253@wp.pl>
2024-05-26 22:43:29 +0200

    Extract separate application on RouteTracker

---
+++ RouteTrackerFlow/RouteTrackerFlowCore/src/main/java/com/warehouse/routetracker/RouteTrackerWebApplication.java
@@ -0,0 +1,25 @@
+package com.warehouse.routetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableConfigurationProperties
+@EnableCaching
+@EnableScheduling
+@SpringBootApplication(scanBasePackages = "com.warehouse.routetracker")
+@EntityScan(basePackages = {"com.warehouse.routetracker"})
+@EnableJpaRepositories(basePackages = {"com.warehouse.routetracker"})
+@ConfigurationPropertiesScan("com.warehouse.routetracker")
+public class RouteTrackerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouteTrackerWebApplication.class, args);
+    }
+
+}
