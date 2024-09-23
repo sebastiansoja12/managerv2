b0bc67fe17144cf9e740722497f6d2c6f2317663
Sebastian Soja <sebastian5253@wp.pl>
2024-09-23 19:35:08 +0200

    Change request and response origin

--- ZebraIntegration/ZebraIntegrationCore/src/main/java/com/warehouse/zebra/infrastructure/adapter/primary/mapper/ZebraRequestMapper.java
+++ ZebraIntegration/ZebraIntegrationCore/src/main/java/com/warehouse/zebra/infrastructure/adapter/primary/mapper/ZebraRequestMapper.java
@@ -4,17 +4,16 @@ package com.warehouse.zebra.infrastructure.adapter.primary.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import com.warehouse.zebra.domain.vo.Request;
-import com.warehouse.zebra.domain.vo.ReturnRequest;
+import com.warehouse.commonassets.request.Request;
+import com.warehouse.commonassets.request.ReturnRequest;
 import com.warehouse.zebra.infrastructure.api.requestmodel.ReturnRequestInformation;
 import com.warehouse.zebra.infrastructure.api.requestmodel.ZebraRequest;
 
 @Mapper
 public interface ZebraRequestMapper {
 
-    @Mapping(target = "returnRequests", source = "requests")
-    Request map(ZebraRequest zebraRequest);
+    Request map(final ZebraRequest zebraRequest);
 
     @Mapping(target = "supplierCode", source = "supplierCode.value")
-    ReturnRequest map(ReturnRequestInformation returnRequestInformation);
+    ReturnRequest map(final ReturnRequestInformation returnRequestInformation);
 }
