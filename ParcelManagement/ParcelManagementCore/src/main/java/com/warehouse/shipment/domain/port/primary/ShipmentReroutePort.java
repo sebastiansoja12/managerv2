c0fbe06d2b619d06b745c346c5125275708a4cff
Sebastian Soja <sebastian5253@wp.pl>
2024-09-29 22:22:17 +0200

    Delete unused classes

--- ParcelManagement/ParcelManagementCore/src/main/java/com/warehouse/shipment/domain/port/primary/ShipmentReroutePort.java
+++
@@ -1,9 +0,0 @@
-package com.warehouse.shipment.domain.port.primary;
-
-import com.warehouse.shipment.domain.vo.UpdateParcelRequest;
-import com.warehouse.shipment.domain.vo.UpdateParcelResponse;
-
-public interface ShipmentReroutePort {
-    UpdateParcelResponse reroute(UpdateParcelRequest updateParcelRequest);
-
-}
