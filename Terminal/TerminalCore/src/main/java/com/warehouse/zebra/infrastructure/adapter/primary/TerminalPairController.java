86016b53b651fdaad9f78b49afd33c77a3be57c7
Sebastian Soja <sebastian5253@wp.pl>
2024-10-24 21:59:21 +0200

    Create TerminalPairController

---
+++ Terminal/TerminalCore/src/main/java/com/warehouse/zebra/infrastructure/adapter/primary/TerminalPairController.java
@@ -0,0 +1,11 @@
+package com.warehouse.zebra.infrastructure.adapter.primary;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/terminals")
+public class TerminalPairController {
+    // TODO
+}
