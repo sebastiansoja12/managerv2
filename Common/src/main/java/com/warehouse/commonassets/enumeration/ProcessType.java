09ed59caf10abcf46bd2df42cbca25baaa7ba593
Sebastian Soja <sebastian5253@wp.pl>
2024-12-02 00:49:11 +0100

    Add new methods for ProcessType

--- Common/src/main/java/com/warehouse/commonassets/enumeration/ProcessType.java
+++ Common/src/main/java/com/warehouse/commonassets/enumeration/ProcessType.java
@@ -8,6 +8,14 @@ public enum ProcessType {
     }
 
     public boolean isCreateProcessType() {
-        return this.equals(CREATED) || this.equals(REDIRECT);
+        return this.equals(CREATED);
+    }
+
+    public boolean isDeliveryProcessType() {
+        return this.equals(ROUTE) || this.equals(MISS);
+    }
+
+    public boolean isReturnProcessType() {
+        return this.equals(RETURN) || this.equals(REJECT);
     }
 }
