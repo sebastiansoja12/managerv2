62c71e186842cb6833fc8cb10aeacfb8167fcdff
Sebastian Soja <sebastian5253@wp.pl>
2024-12-29 17:37:47 +0100

    Add DevicePair test

--- Terminal/TerminalCore/src/test/java/com/warehouse/terminal/DevicePairReadRepositoryTest.java
+++ Terminal/TerminalCore/src/test/java/com/warehouse/terminal/DevicePairReadRepositoryTest.java
@@ -1,4 +1,42 @@
 package com.warehouse.terminal;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.warehouse.commonassets.identificator.DeviceId;
+import com.warehouse.terminal.configuration.DeviceTestConfiguration;
+import com.warehouse.terminal.infrastructure.adapter.secondary.DevicePairReadRepository;
+import com.warehouse.terminal.infrastructure.adapter.secondary.entity.DevicePairEntity;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@ContextConfiguration(classes = DeviceTestConfiguration.class)
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class, TransactionDbUnitTestExecutionListener.class})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class DevicePairReadRepositoryTest {
+
+    @Autowired
+    private DevicePairReadRepository repository;
+
+    @Test
+    void shouldNotFindByDeviceId() {
+        // given
+        final DeviceId deviceId = new DeviceId(1L);
+        // when
+        final Optional<DevicePairEntity> devicePair = repository.findByDevice_DeviceId(deviceId);
+        // then
+        assertFalse(devicePair.isPresent());
+    }
 }
