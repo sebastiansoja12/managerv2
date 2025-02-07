c0f16cc2b84b6a19716f6b3b7095d4d756b339a6
Sebastian Soja <sebastian5152@wp.pl>
2025-02-07 19:47:37 +0100

    Fix sql

--- Application/src/main/resources/migration/2025/2025.1/V2025.1_5_add_device_settings.sql
+++ Application/src/main/resources/migration/2025/2025.1/V2025.1_5_add_device_settings.sql
@@ -1,7 +1,7 @@
 CREATE TABLE device_settings
 (
     device_settings_id        VARCHAR(255) NOT NULL PRIMARY KEY,
-    device_id                 VARCHAR(255) NOT NULL UNIQUE,
+    device_id                 BIGINT NOT NULL UNIQUE,
     cross_courier_delivery    BOOLEAN      NOT NULL,
     validate_responsible_user BOOLEAN      NOT NULL,
     validate_department_code  BOOLEAN      NOT NULL
