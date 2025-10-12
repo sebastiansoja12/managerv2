59d301a2b44a6fa860d3a449dced896d99c7c5f9
Sebastian Soja <sebastian5152@wp.pl>
2025-10-12 15:18:04 +0200

    add validation

--- ReturningTrackManager/ReturningTrackManagerCore/src/main/java/com/warehouse/returning/infrastructure/adapter/primary/validator/ReturnRequestApiValidator.java
+++ ReturningTrackManager/ReturningTrackManagerCore/src/main/java/com/warehouse/returning/infrastructure/adapter/primary/validator/ReturnRequestApiValidator.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 import com.warehouse.returning.domain.helper.Result;
+import com.warehouse.returning.infrastructure.adapter.primary.api.dto.ReturnPackageRequestApi;
 import com.warehouse.returning.infrastructure.adapter.primary.api.dto.ReturnRequestApi;
 
 @Component
@@ -18,6 +19,34 @@ public class ReturnRequestApiValidator extends RequestValidator {
             errors.add("Request must contain at least one package");
         }
 
+        validateRequests(request.requests(), errors);
+
         return !errors.isEmpty() ? Result.failure(errors) : Result.success();
+    }
+
+    private void validateRequests(final List<ReturnPackageRequestApi> requests, final List<String> errors) {
+        requests.forEach(request -> {
+            if (request.shipmentId() == null || request.shipmentId().value() == null) {
+                errors.add("Shipment id must be provided");
+            }
+            if (request.reason() == null) {
+                errors.add("Reason must be provided");
+            }
+            if (request.departmentCode() == null || request.departmentCode().value() == null) {
+                errors.add("Department code must be provided");
+            }
+            if (request.userId() == null || request.userId().value() == null) {
+                errors.add("User id must be provided");
+            }
+            if (request.reasonCode() == null || request.reasonCode().value() == null) {
+                errors.add("Reason code must be provided");
+            } else {
+                try {
+                    com.warehouse.returning.domain.enumeration.ReasonCode.valueOf(request.reasonCode().value());
+                } catch (final IllegalArgumentException e) {
+                    errors.add("Invalid reason code: " + request.reasonCode().value());
+                }
+            }
+        });
     }
 }
