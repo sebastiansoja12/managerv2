41eff7f305183f2bc56c486e5f72a036eece9883
Sebastian Soja <sebastian5152@wp.pl>
2025-10-12 15:19:05 +0200

    add exception

--- ReturningTrackManager/ReturningTrackManagerCore/src/main/java/com/warehouse/returning/infrastructure/adapter/secondary/exception/BusinessException.java
+++ ReturningTrackManager/ReturningTrackManagerCore/src/main/java/com/warehouse/returning/infrastructure/adapter/secondary/exception/BusinessException.java
@@ -1,4 +1,7 @@
 package com.warehouse.returning.infrastructure.adapter.secondary.exception;
 
-public class BusinessException extends RuntimeException{
+public class BusinessException extends RuntimeException {
+    public BusinessException(final String message) {
+        super(message);
+    }
 }
