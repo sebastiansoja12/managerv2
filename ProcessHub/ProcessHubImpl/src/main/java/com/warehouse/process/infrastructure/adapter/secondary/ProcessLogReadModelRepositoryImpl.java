8fbaf64733256c08c9879a5275f80e89d9a50b2b
Sebastian Soja <sebastian5152@wp.pl>
2026-07-14 16:59:35 +0200

    refactor(process): assign operator to read model

--- ProcessHub/ProcessHubImpl/src/main/java/com/warehouse/process/infrastructure/adapter/secondary/ProcessLogReadModelRepositoryImpl.java
+++ ProcessHub/ProcessHubImpl/src/main/java/com/warehouse/process/infrastructure/adapter/secondary/ProcessLogReadModelRepositoryImpl.java
@@ -2,12 +2,16 @@ package com.warehouse.process.infrastructure.adapter.secondary;
 
 import java.util.Optional;
 
+import com.warehouse.commonassets.identificator.OperatorId;
 import com.warehouse.commonassets.identificator.ProcessId;
+import com.warehouse.commonassets.model.UsernameTenantPasswordAuthenticationToken;
 import com.warehouse.process.domain.model.ProcessLog;
 import com.warehouse.process.domain.port.secondary.ProcessLogReadModelRepository;
 import com.warehouse.process.infrastructure.adapter.secondary.entity.read.ProcessLogReadEntity;
 import com.warehouse.process.infrastructure.adapter.secondary.mapper.ProcessLogToModelMapper;
 import com.warehouse.process.infrastructure.adapter.secondary.mapper.ProcessLogToReadEntityMapper;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 public class ProcessLogReadModelRepositoryImpl implements ProcessLogReadModelRepository {
 
@@ -20,6 +24,7 @@ public class ProcessLogReadModelRepositoryImpl implements ProcessLogReadModelRep
     @Override
     public void createProcessLog(final ProcessLog processLog) {
         final ProcessLogReadEntity entity = ProcessLogToReadEntityMapper.map(processLog);
+        assignOperator(entity, getOperatorId());
 
         processLogRepository.save(entity);
     }
@@ -33,6 +38,22 @@ public class ProcessLogReadModelRepositoryImpl implements ProcessLogReadModelRep
     @Override
     public void syncProcessLog(final ProcessLog processLog) {
         final ProcessLogReadEntity entity = ProcessLogToReadEntityMapper.map(processLog);
+        assignOperator(entity, getOperatorId());
         processLogRepository.save(entity);
+    }
+
+    private void assignOperator(final ProcessLogReadEntity entity, final OperatorId operatorId) {
+        entity.assignOperator(operatorId);
+        entity.getCommunicationLogs().forEach(log -> log.assignOperator(operatorId));
+    }
+
+    private OperatorId getOperatorId() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication instanceof UsernameTenantPasswordAuthenticationToken token) {
+            return token.getOperatorId();
+        }
+
+        throw new IllegalStateException("No operator id found");
     }
 }
