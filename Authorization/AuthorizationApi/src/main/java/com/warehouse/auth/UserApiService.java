b67351f3c7b2dbeb7038d2aa3fd1ef185807c0f8
Sebastian Soja <sebastian5152@wp.pl>
2026-07-14 16:56:23 +0200

    feat(auth): define operator user provisioning API

--- Authorization/AuthorizationApi/src/main/java/com/warehouse/auth/UserApiService.java
+++ Authorization/AuthorizationApi/src/main/java/com/warehouse/auth/UserApiService.java
@@ -1,9 +1,11 @@
 package com.warehouse.auth;
 
 import com.warehouse.auth.infrastructure.dto.UserDto;
+import com.warehouse.auth.infrastructure.dto.UserIdDto;
 import com.warehouse.commonassets.identificator.UserId;
 
 public interface UserApiService {
     UserDto findById(final UserId userId);
     UserDto findByUsername(final String username);
+    UserIdDto findInitialUserForOperator();
 }
