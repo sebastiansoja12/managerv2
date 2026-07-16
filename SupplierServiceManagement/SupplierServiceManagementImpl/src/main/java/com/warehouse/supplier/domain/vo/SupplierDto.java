a4d559a8efe24845bd7fc892064613608f80792b
Sebastian Soja <sebastian5152@wp.pl>
2026-07-16 19:39:55 +0200

    Updates supplier handling SupplierDto

--- SupplierServiceManagement/SupplierServiceManagementImpl/src/main/java/com/warehouse/supplier/domain/vo/SupplierDto.java
+++ SupplierServiceManagement/SupplierServiceManagementImpl/src/main/java/com/warehouse/supplier/domain/vo/SupplierDto.java
@@ -1,6 +1,7 @@
 package com.warehouse.supplier.domain.vo;
 
 import com.warehouse.commonassets.enumeration.PackageType;
+import com.warehouse.commonassets.identificator.DepartmentCode;
 import com.warehouse.commonassets.identificator.DeviceId;
 import com.warehouse.commonassets.identificator.VehicleId;
 import com.warehouse.supplier.domain.model.DeliveryArea;
@@ -8,13 +9,13 @@ import com.warehouse.supplier.domain.model.DeliveryArea;
 import java.util.Set;
 
 public record SupplierDto(String firstName, String lastName,
-                          String telephoneNumber, VehicleId vehicleId, DeviceId deviceId,
+                          String telephoneNumber, DepartmentCode departmentCode, VehicleId vehicleId, DeviceId deviceId,
                           DangerousGoodCertification dangerousGoodCertification, DriverLicense driverLicense,
                           DeliveryArea deliveryArea, Set<PackageType> supportedPackageTypes) {
 
     public static SupplierDto from(final SupplierUpdateCommand request) {
         return new SupplierDto(request.firstName(), request.lastName(), request.telephoneNumber(),
-                request.vehicleId(), request.deviceId(), request.dangerousGoodCertification(), request.driverLicense(),
+                request.departmentCode(), request.vehicleId(), request.deviceId(), request.dangerousGoodCertification(), request.driverLicense(),
                 request.deliveryArea(), request.supportedPackageTypes());
     }
 }
