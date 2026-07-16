437407c1bf3073480ef3998101d6c2c692a35e31
Sebastian Soja <sebastian5152@wp.pl>
2026-07-16 19:39:53 +0200

    Updates authentication component AuthenticationPort

--- Authorization/AuthorizationImpl/src/main/java/com/warehouse/auth/domain/port/primary/AuthenticationPort.java
+++ Authorization/AuthorizationImpl/src/main/java/com/warehouse/auth/domain/port/primary/AuthenticationPort.java
@@ -1,7 +1,6 @@
 package com.warehouse.auth.domain.port.primary;
 
 import com.warehouse.auth.domain.model.AdminCreateRequest;
-import com.warehouse.auth.domain.model.RefreshTokenRequest;
 import com.warehouse.auth.domain.model.RegisterRequest;
 import com.warehouse.auth.domain.vo.AuthenticationResponse;
 import com.warehouse.auth.domain.vo.LoginRequest;
@@ -10,8 +9,9 @@ import com.warehouse.commonassets.identificator.UserId;
 
 public interface AuthenticationPort {
     AuthenticationResponse login(final LoginRequest loginRequest);
+    AuthenticationResponse refresh(final String refreshToken);
     RegisterResponse signup(final RegisterRequest registerRequest);
     UserId createAdminUser(final AdminCreateRequest request);
-    void logout(final RefreshTokenRequest refreshTokenRequest);
+    void logout(final String refreshToken);
     void initiatePasswordReset(final String email);
 }
