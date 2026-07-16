4e16767220dbe2f111e4d8765b46cfd830c14631
Sebastian Soja <sebastian5152@wp.pl>
2026-07-16 19:39:55 +0200

    Updates supplier handling SupplierValidatorServiceImpl

--- SupplierServiceManagement/SupplierServiceManagementImpl/src/main/java/com/warehouse/supplier/domain/service/SupplierValidatorServiceImpl.java
+++ SupplierServiceManagement/SupplierServiceManagementImpl/src/main/java/com/warehouse/supplier/domain/service/SupplierValidatorServiceImpl.java
@@ -1,8 +1,10 @@
 package com.warehouse.supplier.domain.service;
 
 import com.warehouse.commonassets.helper.Result;
+import com.warehouse.commonassets.identificator.DepartmentCode;
 import com.warehouse.commonassets.identificator.SupplierCode;
 import com.warehouse.commonassets.identificator.SupplierId;
+import com.warehouse.supplier.domain.port.secondary.DepartmentServicePort;
 import com.warehouse.supplier.domain.port.secondary.DeviceServicePort;
 import com.warehouse.supplier.domain.port.secondary.SupplierRepository;
 import com.warehouse.supplier.domain.vo.SupplierUpdateCommand;
@@ -15,10 +17,14 @@ public class SupplierValidatorServiceImpl implements SupplierValidatorService {
 
     private final DeviceServicePort deviceServicePort;
 
+    private final DepartmentServicePort departmentServicePort;
+
     public SupplierValidatorServiceImpl(final SupplierRepository supplierRepository,
-                                        final DeviceServicePort deviceServicePort) {
+                                        final DeviceServicePort deviceServicePort,
+                                        final DepartmentServicePort departmentServicePort) {
         this.supplierRepository = supplierRepository;
         this.deviceServicePort = deviceServicePort;
+        this.departmentServicePort = departmentServicePort;
     }
 
     @Override
@@ -70,21 +76,29 @@ public class SupplierValidatorServiceImpl implements SupplierValidatorService {
     }
 
     @Override
-    public boolean validateSupplierId(final SupplierId supplierId) {
-        return false;
+    public boolean validateSupplierExistsById(final SupplierId supplierId) {
+        return supplierRepository.findById(supplierId) != null;
     }
 
     @Override
     public Result<Void, String> validateSupplierForUpdate(final SupplierUpdateCommand supplierUpdateCommand) {
         final Result<Void, String> result;
-        final Result<Void, String> deviceValidationResult = deviceServicePort.validateDevice(supplierUpdateCommand.deviceId());
+        final DepartmentCode departmentCode = supplierUpdateCommand.departmentCode();
+        final Result<Void, Void> departmentResult = departmentServicePort.validateDepartmentCode(departmentCode);
+        if (departmentResult.isFailure()) {
+            return Result.failure("Department with code " + departmentCode + " not found");
+        }
         if (supplierRepository.findByCode(supplierUpdateCommand.supplierCode()) == null) {
             result = Result.failure("Supplier with code " + supplierUpdateCommand.supplierCode() + " not found");
-        } else if (deviceValidationResult.isFailure()) {
-            result = deviceValidationResult;
         } else {
             result = Result.success();
         }
         return result;
+    }
+
+    @Override
+    public Boolean validateDepartmentExistsByDeparmtneCode(final DepartmentCode departmentCode) {
+        final Result<Void, Void> result = departmentServicePort.validateDepartmentCode(departmentCode);
+        return result.isSuccess();
     }
 }
