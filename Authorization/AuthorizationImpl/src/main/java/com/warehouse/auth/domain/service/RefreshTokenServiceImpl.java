09d8f4b2ba70a516c3d6d84bf12bd7e971e667da
Sebastian Soja <sebastian5152@wp.pl>
2026-07-16 19:39:53 +0200

    Updates authentication component RefreshTokenServiceImpl

--- Authorization/AuthorizationImpl/src/main/java/com/warehouse/auth/domain/service/RefreshTokenServiceImpl.java
+++ Authorization/AuthorizationImpl/src/main/java/com/warehouse/auth/domain/service/RefreshTokenServiceImpl.java
@@ -1,9 +1,7 @@
 package com.warehouse.auth.domain.service;
 
-import com.warehouse.auth.domain.model.RefreshToken;
 import com.warehouse.auth.domain.port.secondary.RefreshTokenRepository;
 
-import com.warehouse.commonassets.identificator.UserId;
 import lombok.AllArgsConstructor;
 
 import java.time.LocalDateTime;
@@ -12,21 +10,6 @@ import java.time.LocalDateTime;
 public class RefreshTokenServiceImpl implements RefreshTokenService {
 
     private final RefreshTokenRepository refreshTokenRepository;
-
-    @Override
-    public RefreshToken validateRefreshToken(String token) {
-        return refreshTokenRepository.validateRefreshToken(token);
-    }
-
-    @Override
-    public void deleteRefreshToken(final UserId userId, final String token) {
-        refreshTokenRepository.delete(token);
-    }
-
-    @Override
-    public RefreshToken findTokenByUserId(final UserId userId) {
-        return refreshTokenRepository.findByUserId(userId);
-    }
 
     @Override
     public void delete(final LocalDateTime now) {
