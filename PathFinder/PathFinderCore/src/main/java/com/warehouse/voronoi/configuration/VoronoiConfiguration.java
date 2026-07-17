bbd58bae4613334d87522e92ed730ea725b9de44
Sebastian Soja <sebastian5152@wp.pl>
2026-07-17 16:36:17 +0200

    refactor voronoi config

--- PathFinder/PathFinderCore/src/main/java/com/warehouse/voronoi/configuration/VoronoiConfiguration.java
+++ PathFinder/PathFinderCore/src/main/java/com/warehouse/voronoi/configuration/VoronoiConfiguration.java
@@ -1,24 +1,29 @@
 package com.warehouse.voronoi.configuration;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.warehouse.infrastructure.GeocodingApiService;
 import com.warehouse.positionstack.PositionStackProperties;
 import com.warehouse.voronoi.VoronoiCoordinatesService;
 import com.warehouse.voronoi.VoronoiService;
 import com.warehouse.voronoi.domain.port.primary.VoronoiPort;
 import com.warehouse.voronoi.domain.port.primary.VoronoiPortImpl;
+import com.warehouse.voronoi.domain.port.secondary.GeocodingConfigServicePort;
+import com.warehouse.voronoi.domain.port.secondary.GeolocationServiceProvider;
 import com.warehouse.voronoi.domain.port.secondary.PositionStackRepository;
-import com.warehouse.voronoi.domain.port.secondary.VoronoiServicePort;
 import com.warehouse.voronoi.domain.service.ComputeService;
 import com.warehouse.voronoi.domain.service.ComputeServiceImpl;
 import com.warehouse.voronoi.domain.service.UrlJsonReaderService;
 import com.warehouse.voronoi.domain.service.UrlReaderServiceImpl;
 import com.warehouse.voronoi.infrastructure.adapter.primary.VoronoiServiceAdapter;
+import com.warehouse.voronoi.infrastructure.adapter.secondary.GeocodingConfigServiceAdapter;
+import com.warehouse.voronoi.infrastructure.adapter.secondary.GeolocationAdapter;
 import com.warehouse.voronoi.infrastructure.adapter.secondary.PositionStackReadRepository;
 import com.warehouse.voronoi.infrastructure.adapter.secondary.PositionStackRepositoryImpl;
-import com.warehouse.voronoi.infrastructure.adapter.secondary.VoronoiAdapter;
 
 @Configuration
 public class VoronoiConfiguration {
@@ -32,14 +37,20 @@ public class VoronoiConfiguration {
 
 	@Bean
 	public VoronoiPort voronoiPort(final ComputeService computeService,
-                                   final VoronoiServicePort voronoiServicePort) {
-		return new VoronoiPortImpl(computeService, voronoiServicePort);
+                                   final Set<GeolocationServiceProvider> geolocationServiceProvider,
+                                   final GeocodingConfigServicePort configServicePort) {
+		return new VoronoiPortImpl(computeService, geolocationServiceProvider, configServicePort);
 	}
 
+    @Bean
+    public GeocodingConfigServicePort geocodingConfigServicePort(final GeocodingApiService geocodingApiService) {
+        return new GeocodingConfigServiceAdapter(geocodingApiService);
+    }
+
 	@Bean
-	public VoronoiServicePort voronoiServicePort(final PositionStackProperties positionStackProperties,
-                                                 final PositionStackRepository positionStackRepository) {
-		return new VoronoiAdapter(positionStackProperties, positionStackRepository);
+	public GeolocationServiceProvider voronoiServicePort(final PositionStackProperties positionStackProperties,
+                                                         final PositionStackRepository positionStackRepository) {
+		return new GeolocationAdapter(positionStackProperties, positionStackRepository);
 	}
 
     @Bean
@@ -59,7 +70,8 @@ public class VoronoiConfiguration {
     }
 
     @Bean
-    public ComputeService computeService(final VoronoiServicePort voronoiServicePort) {
-        return new ComputeServiceImpl(voronoiServicePort);
+    public ComputeService computeService(final Set<GeolocationServiceProvider> geolocationServiceProvider,
+                                         final GeocodingConfigServicePort geocodingConfigServicePort) {
+        return new ComputeServiceImpl(geolocationServiceProvider, geocodingConfigServicePort);
     }
 }
