1196a0259a7ec30d66724afe46e315f80cdf03a7
Sebastian Soja <sebastian5152@wp.pl>
2026-07-17 16:42:10 +0200

    refactor voronoi port

--- PathFinder/PathFinderCore/src/main/java/com/warehouse/voronoi/domain/port/primary/VoronoiPortImpl.java
+++ PathFinder/PathFinderCore/src/main/java/com/warehouse/voronoi/domain/port/primary/VoronoiPortImpl.java
@@ -1,15 +1,19 @@
 package com.warehouse.voronoi.domain.port.primary;
 
 import java.util.List;
+import java.util.Set;
 
+import com.warehouse.commonassets.enumeration.GeocodingProvider;
 import com.warehouse.commonassets.identificator.DepartmentCode;
 import com.warehouse.voronoi.domain.exception.MissingDepotsException;
 import com.warehouse.voronoi.domain.exception.MissingRequestCityException;
 import com.warehouse.voronoi.domain.model.Coordinates;
 import com.warehouse.voronoi.domain.model.Department;
 import com.warehouse.voronoi.domain.model.VoronoiRequest;
-import com.warehouse.voronoi.domain.port.secondary.VoronoiServicePort;
+import com.warehouse.voronoi.domain.port.secondary.GeocodingConfigServicePort;
+import com.warehouse.voronoi.domain.port.secondary.GeolocationServiceProvider;
 import com.warehouse.voronoi.domain.service.ComputeService;
+import com.warehouse.voronoi.domain.vo.GeocodingConfig;
 import com.warehouse.voronoi.domain.vo.VoronoiResponse;
 
 import lombok.extern.slf4j.Slf4j;
@@ -19,12 +23,16 @@ public class VoronoiPortImpl implements VoronoiPort {
 
     private final ComputeService computeService;
 
-    private final VoronoiServicePort voronoiServicePort;
+    private final Set<GeolocationServiceProvider> geolocationServiceProvider;
+
+    private final GeocodingConfigServicePort geocodingConfigServicePort;
 
     public VoronoiPortImpl(final ComputeService computeService,
-                           final VoronoiServicePort voronoiServicePort) {
+                           final Set<GeolocationServiceProvider> geolocationServiceProvider,
+                           final GeocodingConfigServicePort geocodingConfigServicePort) {
         this.computeService = computeService;
-        this.voronoiServicePort = voronoiServicePort;
+        this.geolocationServiceProvider = geolocationServiceProvider;
+        this.geocodingConfigServicePort = geocodingConfigServicePort;
     }
 
     @Override
@@ -39,7 +47,13 @@ public class VoronoiPortImpl implements VoronoiPort {
 
     @Override
     public Coordinates obtainCoordinates(final String requestCity) {
-        return voronoiServicePort.obtainCoordinates(requestCity);
+        final GeocodingConfig geocodingConfig = geocodingConfigServicePort.findGeocodingConfig(GeocodingProvider.POSITION_STACK);
+        return geolocationServiceProvider.stream()
+                .filter(e -> geocodingConfig != null)
+                .filter(provider -> provider.canHandle(geocodingConfig.provider()))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("External system for finding geolocations not configured for this operator!!!"))
+                .obtainCoordinates(requestCity);
     }
 
     private void validateRequest(final List<Department> departments, final String requestCity) {
