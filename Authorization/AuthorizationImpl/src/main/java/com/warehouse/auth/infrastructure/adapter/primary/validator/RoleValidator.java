bb8ce78b93c63c14605cfbce21cdeda2f68d978e
Sebastian Soja <sebastian5152@wp.pl>
2026-07-17 16:12:52 +0200

    refactor user controller

--- Authorization/AuthorizationImpl/src/main/java/com/warehouse/auth/infrastructure/adapter/primary/validator/RoleValidator.java
+++ Authorization/AuthorizationImpl/src/main/java/com/warehouse/auth/infrastructure/adapter/primary/validator/RoleValidator.java
@@ -1,21 +1,22 @@
 package com.warehouse.auth.infrastructure.adapter.primary.validator;
 
-import com.warehouse.auth.domain.helper.Result;
-import com.warehouse.auth.domain.model.User;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import com.warehouse.auth.domain.helper.Result;
+import com.warehouse.auth.domain.model.User;
+import com.warehouse.auth.infrastructure.adapter.secondary.entity.UserEntity;
 
 public class RoleValidator {
 
     public Result<Void, List<String>> validatePermission(final String permission) {
         final List<String> errors = new ArrayList<>();
         if (permission == null || permission.isBlank()) {
-            errors.add("Permission name cannot be null or empty");
+            errors.add("UserPermission name cannot be null or empty");
         }
 
         try {
-            User.Permission.valueOf(permission);
+            UserEntity.Permission.valueOf(permission);
         } catch (final IllegalArgumentException e) {
             errors.add("Unknown permission: " + permission);
         }
