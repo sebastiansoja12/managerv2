48565df9a0192f5bb8c82aa888f92b73c050b165
Sebastian Soja <sebastian5152@wp.pl>
2026-07-17 13:45:53 +0200

    refactor geocoding snapshot

---
+++ GeocodingService/GeocodingServiceImpl/src/main/java/com/warehouse/geocoding/domain/vo/GeocodingConfigurationSnapshot.java
@@ -0,0 +1,15 @@
+package com.warehouse.geocoding.domain.vo;
+
+import com.warehouse.commonassets.enumeration.GeocodingProvider;
+import com.warehouse.commonassets.identificator.GeocodingConfigurationId;
+
+public record GeocodingConfigurationSnapshot(GeocodingConfigurationId geocodingConfigurationId,
+                                             String apiUrl,
+                                             String apiUserName,
+                                             String apiPassword,
+                                             String apiKey,
+                                             String accessToken,
+                                             String refreshToken,
+                                             boolean enabled,
+                                             GeocodingProvider provider) {
+}
