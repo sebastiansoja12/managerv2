b18b2f9f5109df3e1ca63be921317c0f2541a747
Sebastian Soja <sebastian5152@wp.pl>
2026-07-30 19:01:29 +0200

    remove dangerous good module

--- ShipmentManagement/ShipmentManagementImpl/src/main/java/com/warehouse/dangerousgood/domain/service/ShipmentService.java
+++
@@ -1,8 +0,0 @@
-package com.warehouse.dangerousgood.domain.service;
-
-import com.warehouse.commonassets.identificator.ShipmentId;
-import com.warehouse.dangerousgood.domain.vo.Shipment;
-
-public interface ShipmentService {
-    Shipment findById(final ShipmentId shipmentId);
-}
--- ShipmentManagement/ShipmentManagementImpl/src/main/java/com/warehouse/dangerousgood/infrastructure/adapter/primary/api/WeightApi.java
+++
@@ -1,6 +0,0 @@
-package com.warehouse.dangerousgood.infrastructure.adapter.primary.api;
-
-import java.math.BigDecimal;
-
-public record WeightApi(BigDecimal value, String unit) {
-}
