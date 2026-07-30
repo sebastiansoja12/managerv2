8a62d81469d1eabb72d17b43b34a897f21d34c88
Sebastian Soja <sebastian5152@wp.pl>
2026-07-30 19:02:20 +0200

    update department code equality

--- Common/src/main/java/com/warehouse/commonassets/identificator/DepartmentCode.java
+++ Common/src/main/java/com/warehouse/commonassets/identificator/DepartmentCode.java
@@ -1,5 +1,7 @@
 package com.warehouse.commonassets.identificator;
 
+import java.util.Objects;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
@@ -17,8 +19,24 @@ public class DepartmentCode implements ObjectValue<String> {
 
     public String value() {return value;}
     public String getValue() { return value; }
-    public void setValue(String value) { this.value = value; }
+    public void setValue(final String value) { this.value = value; }
 
     @Override
     public String toString() { return value; }
+
+    @Override
+    public boolean equals(final Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof DepartmentCode that)) {
+            return false;
+        }
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
