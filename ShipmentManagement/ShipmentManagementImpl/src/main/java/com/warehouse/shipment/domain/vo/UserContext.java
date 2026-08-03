02c2998d9282718122d4570cbdd2b7106fa9ffd3
Sebastian Soja <sebastian5152@wp.pl>
2026-08-03 11:06:00 +0200

    Move shipment API handling to shared context models

--- ShipmentManagement/ShipmentManagementImpl/src/main/java/com/warehouse/shipment/domain/vo/UserContext.java
+++
@@ -1,7 +0,0 @@
-package com.warehouse.shipment.domain.vo;
-
-import com.warehouse.commonassets.identificator.DepartmentCode;
-import com.warehouse.commonassets.identificator.UserId;
-
-public record UserContext(UserId userId, DepartmentCode departmentCode) {
-}
