4325fd85cdab5e2ea220e3b6293d25000d208d4a
Sebastian Soja <sebastian5152@wp.pl>
2026-08-08 21:21:00 +0200

    Delete supplier terminal and username ports

--- RouteLogger/RouteLoggerCore/src/main/java/com/warehouse/routelogger/domain/port/secondary/RouteLoggerUsernameServicePort.java
+++
@@ -1,7 +0,0 @@
-package com.warehouse.routelogger.domain.port.secondary;
-
-import com.warehouse.routelogger.domain.model.UsernameLogRequest;
-
-public interface RouteLoggerUsernameServicePort {
-    void logUsername(UsernameLogRequest request);
-}
