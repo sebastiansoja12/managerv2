f3e8299ec7888ce6ba5f311e213bc328a0da49ba
Sebastian Soja <sebastian5152@wp.pl>
2026-08-09 07:28:00 +0200

    Remove the RouteLogger listener and version port

--- RouteLogger/RouteLoggerCore/src/main/java/com/warehouse/routelogger/domain/port/secondary/RouteLoggerVersionServicePort.java
+++
@@ -1,7 +0,0 @@
-package com.warehouse.routelogger.domain.port.secondary;
-
-import com.warehouse.routelogger.domain.model.VersionLogRequest;
-
-public interface RouteLoggerVersionServicePort {
-    void logVersion(VersionLogRequest request);
-}
