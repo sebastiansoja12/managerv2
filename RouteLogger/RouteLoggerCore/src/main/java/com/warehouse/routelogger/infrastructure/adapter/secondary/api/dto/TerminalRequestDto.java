26f0eac504e12b6160dad1c5919393a7c38f43c8
Sebastian Soja <sebastian5152@wp.pl>
2026-08-10 07:24:00 +0200

    Remove supplier and terminal transport DTOs

--- RouteLogger/RouteLoggerCore/src/main/java/com/warehouse/routelogger/infrastructure/adapter/secondary/api/dto/TerminalRequestDto.java
+++
@@ -1,7 +0,0 @@
-package com.warehouse.routelogger.infrastructure.adapter.secondary.api.dto;
-
-import com.warehouse.routelogger.dto.ShipmentIdDto;
-
-
-public record TerminalRequestDto(String request, ShipmentIdDto shipmentId, String processType) {
-}
