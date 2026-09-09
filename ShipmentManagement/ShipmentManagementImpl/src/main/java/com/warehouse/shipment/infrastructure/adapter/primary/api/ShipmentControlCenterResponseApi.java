1d96fb0262223133a840db4f1d09798ddf22e18a
Sebastian Soja <sebastian5152@wp.pl>
2026-09-10 01:37:00 +0200

    update shipment control center response API and shipment create request API

--- ShipmentManagement/ShipmentManagementImpl/src/main/java/com/warehouse/shipment/infrastructure/adapter/primary/api/ShipmentControlCenterResponseApi.java
+++ ShipmentManagement/ShipmentManagementImpl/src/main/java/com/warehouse/shipment/infrastructure/adapter/primary/api/ShipmentControlCenterResponseApi.java
@@ -2,5 +2,6 @@ package com.warehouse.shipment.infrastructure.adapter.primary.api;
 
 import com.warehouse.shipment.domain.vo.RouteLogRecord;
 
-public record ShipmentControlCenterResponseApi(ShipmentDto shipment, RouteLogRecord routeLog) {
+public record ShipmentControlCenterResponseApi(
+        ShipmentDto shipment, RouteLogRecord routeLog, ShipmentReturnDetailsApi returnPackage) {
 }
--- ShipmentManagement/ShipmentManagementImpl/src/main/java/com/warehouse/shipment/infrastructure/adapter/primary/api/ShipmentCreateRequestApi.java
+++ ShipmentManagement/ShipmentManagementImpl/src/main/java/com/warehouse/shipment/infrastructure/adapter/primary/api/ShipmentCreateRequestApi.java
@@ -1,6 +1,10 @@
 package com.warehouse.shipment.infrastructure.adapter.primary.api;
 
+import com.warehouse.commonassets.identificator.PickupPointId;
+
 public record ShipmentCreateRequestApi(PersonApi sender, PersonApi recipient, ShipmentSizeDto shipmentSize,
 									   MoneyApi price, DangerousGoodApi dangerousGood, ShipmentPriorityDto shipmentPriority,
-									   String issuerCountryCode, String receiverCountryCode) {
+                                       String issuerCountryCode, String receiverCountryCode,
+                                       PickupMethodDto pickupMethod, DeliveryMethodDto deliveryMethod,
+                                       PickupPointId pickupPointId, PickupPointId deliveryPickupPointId) {
 }
