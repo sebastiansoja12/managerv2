bb4de7f7729ac1457e0fb5a79de4aaba524384f6
Sebastian Soja <sebastian5152@wp.pl>
2026-09-09 19:19:00 +0200

    update returning service port

--- ShipmentManagement/ShipmentManagementImpl/src/main/java/com/warehouse/shipment/application/port/secondary/ReturningServicePort.java
+++ ShipmentManagement/ShipmentManagementImpl/src/main/java/com/warehouse/shipment/application/port/secondary/ReturningServicePort.java
@@ -1,11 +1,17 @@
 package com.warehouse.shipment.application.port.secondary;
 
+import java.util.Optional;
+
 import com.warehouse.commonassets.identificator.DepartmentCode;
 import com.warehouse.commonassets.identificator.ReturnId;
+import com.warehouse.commonassets.identificator.ShipmentId;
 import com.warehouse.shipment.domain.vo.ShipmentReturnDetails;
 import com.warehouse.shipment.domain.vo.ShipmentReturnPage;
 
 public interface ReturningServicePort {
     ShipmentReturnDetails getReturn(final ReturnId returnId);
+    Optional<ShipmentReturnDetails> findReturnByShipmentId(final ShipmentId shipmentId);
     ShipmentReturnPage getReturns(final DepartmentCode departmentCode, final int page, final int size);
+    void startProcessing(final ShipmentId shipmentId);
+    void complete(final ShipmentId shipmentId);
 }
